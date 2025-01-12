da65881b99c997d72e2c20da29b4513726f137c8
mikethecoder2005 <Huynh.Michael@student.greenriver.edu>
2025-01-12 09:52:57 -0800

    New Code

---
+++ Assignments 1-5/Hello-World.py
@@ -0,0 +1,11 @@
+
+name = input("Insert your name here: ")
+
+print("Hello", name)
+
+print("Today is going to be a great day!")
+
+age = input("How old will you be this year?")
+
+print("Congradulations for turning", age, "I wish you the very best for the upcoming years, from Mike")
+
